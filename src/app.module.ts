da017fed51c5a921e945e0d3f8a13f790cdca552
mamunsarkarleemon <mamunsarkarleemon@gmail.com>
2025-07-27 00:09:40 +0600

    add user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AdminModule } from './admin/admin.module';
 import { SellerController } from './seller/seller.controller';
 import { SellerModule } from './seller/seller.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AdminModule, SellerModule],
-  controllers: [SellerController],
+  imports: [AdminModule, UserModule],
+  controllers: [UserController],
   providers: [],
 })
 export class AppModule {}
