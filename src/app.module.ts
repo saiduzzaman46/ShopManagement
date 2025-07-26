3da46cc0c097ef52ff36e5ac3f7d6c6fb5865ece
mamunsarkarleemon <mamunsarkarleemon@gmail.com>
2025-07-27 00:16:11 +0600

    add customer

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { SellerController } from './seller/seller.controller';
 import { SellerModule } from './seller/seller.module';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { CustomerController } from './customer/customer.controller';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [AdminModule, UserModule],
-  controllers: [UserController],
+  imports: [AdminModule, UserModule, CustomerModule],
+  controllers: [UserController, CustomerController],
   providers: [],
 })
 export class AppModule {}
